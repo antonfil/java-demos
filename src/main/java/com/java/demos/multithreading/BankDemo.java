f614400b5c4190201cee141436467cfa8ddd238a
antonfil <fil.anton@gmail.com>
2025-12-21 20:53:33 +0200

    Initial coomit

---
+++ src/main/java/com/java/demos/multithreading/BankDemo.java
@@ -0,0 +1,88 @@
+package com.java.demos.multithreading;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BankDemo {
+	private static final int ACCOUNTS_COUNT = 10;
+	private static final int SINGLE_ACCOUNT_INITIAL_BALANCE = 100_000;
+	
+    private List<Account> accounts = new ArrayList<>(ACCOUNTS_COUNT);
+    private Set<Account> lockedAccounts = new HashSet<>();
+    
+    public BankDemo() {
+    	for (int i = 0; i < ACCOUNTS_COUNT; i++) {
+    		accounts.add(new Account(SINGLE_ACCOUNT_INITIAL_BALANCE));
+    	}
+    }
+    
+    public List<Account> getAccounts() {
+    	return accounts;
+    }
+    
+    public int getTotalBalance() {
+    	return accounts.stream().mapToInt(acc -> acc.getBalance()).sum();
+    }
+    
+    public void transfer(Account from, Account to, int amount) throws InterruptedException {
+    	try {
+    		lockAccounts(from, to);
+    		
+    		from.withdraw(amount);
+    		Thread.sleep(100); //Simulate long duration
+    		to.deposit(amount);
+    	} finally {
+    		unlockAccounts(from, to);
+    	}
+    }
+    
+    public synchronized void transfer_1(Account from, Account to, int amount) throws InterruptedException {
+		from.withdraw(amount);
+		Thread.sleep(100); //Simulate long duration
+		to.deposit(amount);
+    }
+    
+    private void lockAccounts(Account from, Account to) throws InterruptedException {
+    	synchronized (lockedAccounts) {
+    		while(lockedAccounts.contains(from) || lockedAccounts.contains(to)) {
+    			lockedAccounts.wait();
+    		}
+    		lockedAccounts.add(from);
+    		lockedAccounts.add(to);
+    	}
+    }
+    
+    private void unlockAccounts(Account from, Account to) {
+    	synchronized (lockedAccounts) {
+    		lockedAccounts.remove(from);
+    		lockedAccounts.remove(to);
+    		lockedAccounts.notifyAll();
+    	}
+    }
+}
+
+class Account {
+	private int balance;
+	
+	public Account(int balance) {
+		this.balance = balance;
+	}
+	
+	public int getBalance() {
+		return balance;
+	}
+	
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	public void deposit(int amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(int amount) {
+		balance -= amount;
+	}
+}
---
+++ src/test/java/com/java/demos/multithreading/BankDemoTest.java
@@ -0,0 +1,69 @@
+package com.java.demos.multithreading;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.junit.jupiter.api.Test;
+
+
+@FunctionalInterface
+interface TransferOp {
+    void transfer(Account from, Account to, int amount) throws InterruptedException;
+}
+
+
+/**
+ * Unit tests for BankDemo
+ */
+public class BankDemoTest {
+	private static final int TEST_TRANSFERS_COUNT = 100;
+
+    @Test
+    public void shouldSyncTransfersBetweenAccounts() throws Exception {
+    	BankDemo bank = new BankDemo(); 
+        int initialTotalBalance = bank.getTotalBalance();
+        
+        runTransfers(bank, bank::transfer);
+
+        int finalTotalBalance = bank.getTotalBalance();
+        assertEquals(initialTotalBalance, finalTotalBalance);
+    }
+    
+    @Test
+    public void shouldSyncTransfersBetweenAccounts_1() throws Exception {
+    	BankDemo bank = new BankDemo(); 
+        int initialTotalBalance = bank.getTotalBalance();
+        
+        runTransfers(bank, bank::transfer_1);
+
+        int finalTotalBalance = bank.getTotalBalance();
+        assertEquals(initialTotalBalance, finalTotalBalance);
+    }
+    
+    private void runTransfers(BankDemo bank, TransferOp operation) throws Exception {
+		ExecutorService executorService = Executors.newFixedThreadPool(TEST_TRANSFERS_COUNT);
+        List<Future<?>> futures = new ArrayList<>(TEST_TRANSFERS_COUNT); 
+        for (int i = 0; i < TEST_TRANSFERS_COUNT; i++) {
+        	futures.add(executorService.submit(() -> {
+        		Random rand = new Random();
+        		int fromAccountIndex = rand.nextInt(bank.getAccounts().size());
+        		int toAccountIndex = rand.nextInt(bank.getAccounts().size());
+        		Account from = bank.getAccounts().get(fromAccountIndex);
+        		Account to = bank.getAccounts().get(toAccountIndex);
+        		int amount = from.getBalance() / 2;
+        		operation.transfer(from, to, amount);
+        		return true;
+        	}));
+        }
+        executorService.shutdown();
+        for (Future<?> f : futures) {
+            f.get();
+        }
+	}
+}
